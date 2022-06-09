5d405503f6d9b33bed2c7f6977347202f3923112
zodegree <mx@unity-mail.de>
2022-06-09 10:19:15 +0200

    Prettier adapted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        fontFamily: {
-            'gabriela': ['Gabriela', 'serif'],
-        },
-        extend: {
-            colors: {
-                'cl1': '#5F7161',
-                'cl2': '#EFEAD8',
-                'cl3': '#D0C9C0',
-                'cl4': '#F1EEE4',
-                'cl5': '#C16666',
-                'cl6': '#BBC9BD',
-                'cl7': '#CE9999',
-            }
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      gabriela: ['Gabriela', 'serif'],
     },
-    plugins: [],
+    extend: {
+      colors: {
+        cl1: '#5F7161',
+        cl2: '#EFEAD8',
+        cl3: '#D0C9C0',
+        cl4: '#F1EEE4',
+        cl5: '#C16666',
+        cl6: '#BBC9BD',
+        cl7: '#CE9999',
+      },
+    },
+  },
+  plugins: [],
 }
