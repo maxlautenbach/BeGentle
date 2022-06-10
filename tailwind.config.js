5d053b569cb70f6637c3d0929be84b775465e7a7
maxlautenbach <77662997+maxlautenbach@users.noreply.github.com>
2022-06-10 13:44:26 +0200

    Homepage + Instrumenten Kachel (#2)
    * Banner finished (Desktop)
    
    * Prettier adapted
    
    * Homepage finished (Frontend)
    
    * Prettier
    
    * Hydration fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      gabriela: ['Gabriela', 'serif'],
+    },
+    extend: {
+      colors: {
+        cl1: '#5F7161',
+        cl2: '#EFEAD8',
+        cl3: '#D0C9C0',
+        cl4: '#F1EEE4',
+        cl5: '#C16666',
+        cl6: '#BBC9BD',
+        cl7: '#CE9999',
+      },
+    },
   },
   plugins: [],
 }
